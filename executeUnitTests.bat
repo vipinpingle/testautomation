272a42beb7d1266bd7abfbe91c8584df3a4681e2
vipinpingle <pinglevipin@gmail.com>
2018-05-02 16:36:09 -0400

    adding new files

--- executeUnitTests.bat
+++ executeUnitTests.bat
@@ -1,2 +1,2 @@
 pytest C:\Jenkins\workspace\py-test\tests\*.py
-python3 -m coverage xml -o C:\Jenkins\workspace\py-test\py-test\coverage.xml
+python -m coverage xml -o C:\Jenkins\workspace\py-test\py-test\coverage.xml
